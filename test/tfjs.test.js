6b3e07a55601ba378a2cb9cc2837b53e7911f8d9
mhkeller <code@mhkeller.com>
2022-12-07 16:31:29 -0500

    clean up

--- test/tfjs.test.js
+++ test/tfjs.test.js
@@ -1,23 +1,11 @@
-import { readDataSync } from 'indian-ocean';
-import * as aq from 'arquero';
-
 import plot from '../src/plot.js';
 
-// const events = readDataSync('./test/data/purchase_data.csv');
-
-// const data = aq.from(events)
-// 	.derive({date: aq.escape(d => new Date(d.date.split('T')[0]))})
-// 	.groupby('date', 'brand')
-// 	.rollup({value: d => aq.op.sum(d.price_in_usd), count: aq.op.count()})
-// 	.orderby('date', 'brand')
-// 	.objects();
-
 const series1 = Array(100).fill(0)
-	.map(y => Math.random() * 100 - (Math.random() * 50))
+	.map(() => Math.random() * 100 - (Math.random() * 50))
 	.map((y, x) => ({ x, y, }));
 
 const series2 = Array(100).fill(0)
-	.map(y => Math.random() * 100 - (Math.random() * 150))
+	.map(() => Math.random() * 100 - (Math.random() * 150))
 	.map((y, x) => ({ x, y, }));
 
 const series = ['First', 'Second'];
@@ -27,6 +15,7 @@ const surface = { name: 'Scatterplot', tab: 'Charts' };
 
 
 const chart = (s, ds) => {
+	// eslint-disable-next-line no-undef
 	tfvis.render.scatterplot(s, ds);
 }
 
