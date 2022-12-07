9bd08fa9a4a56607ac1d2e6f7bdde37dd2b82fb1
mhkeller <code@mhkeller.com>
2022-12-07 16:30:50 -0500

    tfjs example

--- test/tfjs.test.js
+++ test/tfjs.test.js
@@ -1,11 +1,9 @@
 import { readDataSync } from 'indian-ocean';
-// import Plotly from 'plotly.js-dist-min';
 import * as aq from 'arquero';
-import * as tfvis from '../vendor/tfjs-vis.umd.min.js'
 
+import plot from '../src/plot.js';
 
-
-const events = readDataSync('./test/data/purchase_data.csv');
+// const events = readDataSync('./test/data/purchase_data.csv');
 
 // const data = aq.from(events)
 // 	.derive({date: aq.escape(d => new Date(d.date.split('T')[0]))})
@@ -14,24 +12,27 @@ const events = readDataSync('./test/data/purchase_data.csv');
 // 	.orderby('date', 'brand')
 // 	.objects();
 
-// const series1 = Array(100).fill(0)
-// 	.map(y => Math.random() * 100 - (Math.random() * 50))
-// 	.map((y, x) => ({ x, y, }));
+const series1 = Array(100).fill(0)
+	.map(y => Math.random() * 100 - (Math.random() * 50))
+	.map((y, x) => ({ x, y, }));
 
-// const series2 = Array(100).fill(0)
-// 	.map(y => Math.random() * 100 - (Math.random() * 150))
-// 	.map((y, x) => ({ x, y, }));
+const series2 = Array(100).fill(0)
+	.map(y => Math.random() * 100 - (Math.random() * 150))
+	.map((y, x) => ({ x, y, }));
 
-// const series = ['First', 'Second'];
-// const data = { values: [series1, series2], series }
+const series = ['First', 'Second'];
+const data = { values: [series1, series2], series }
 
-// const surface = { name: 'Scatterplot', tab: 'Charts' };
+const surface = { name: 'Scatterplot', tab: 'Charts' };
 
 
-// const chart = async () => tfvis.render.scatterplot(surface, data);
+const chart = (s, ds) => {
+	tfvis.render.scatterplot(s, ds);
+}
 
-// await drawPlot(chart, {
-// 	outPath: 'test/tmp/tensorflow-scatter.png',
-// 	view: true,
-// 	title: 'Scatterplot'
-// });
+await plot(chart, [surface, data], {
+	library: 'tfjs',
+	outPath: 'test/tmp/tensorflow-scatter.png',
+	view: true,
+	title: 'Scatterplot'
+});
